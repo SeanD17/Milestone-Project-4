fd7bb6d3835d1a1347449545ba802910042aa9ed
Sean Dolan <seandolan53@yahoo.com>
2021-12-01 21:30:21 +0000

    Added code to set DEBUG to TRUE only if it is declared inside the env.py file

--- devmuscles/settings.py
+++ devmuscles/settings.py
@@ -22,9 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-
-DEBUG = 'TRUE'
+ 
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['milestone-project-four-muscles.herokuapp.com', 'localhost']
 
