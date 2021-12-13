af1b5e232406a664d4aff7169b38dea506b110cc
Sean Dolan <seandolan53@yahoo.com>
2021-12-13 11:32:13 +0000

    Finished the blog app

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,6 @@ class PostList(generic.ListView):
     """
     Return all posts that are with status 1 (published) and order from the latest one.
     """
-    queryset = Post.objects.filter(status=1).order_by('-created_at')
-    template_name = 'blog.html'
+    queryset = Post.objects.filter(status=1).order_by('title')
+    template_name = 'blog/blog.html'
 
-
-class PostDetail(generic.DetailView):
-    model = Post
-    template_name = 'post_detail.html'
